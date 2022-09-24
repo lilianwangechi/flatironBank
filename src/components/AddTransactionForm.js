4a5efa67c3ca22f21242285239a74b0d75d477d0
lilian <lilian.wangechi@student.moringaschool.com>
2022-09-25 02:46:43 +0300

    add value attribute to the form elemets

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -3,10 +3,10 @@ import React, {useState} from "react";
 function AddTransactionForm() {
 //adding a new transaction
 const[formData,setFormData] =useState({
-Date:"",
-Description:"",
-Category:"",
-Amount:""
+date:"",
+description:"",
+category:"",
+amount:""
 })
 
 function handleChange(event){
@@ -16,16 +16,16 @@ function handleChange(event){
 
 setFormData({
   ...formData,
-  [name].value
+  [name]:value
 });
   return (
     <div className="ui segment">
       <form className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" value={formData.date}/>
+          <input type="text" name="description" value={formData.description} placeholder="Description" />
+          <input type="text" name="category" value={formData.category} placeholder="Category" />
+          <input type="number" name="amount"value={formData.amount}  placeholder="Amount" step="0.01" />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
