ad27c3def8e1df469a7b7f5affc03b117b980e35
lilian <lilian.wangechi@student.moringaschool.com>
2022-09-27 05:06:19 +0300

    search function has a bug

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React ,{useState,useEffect}from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+const [transaction,setTransaction] =useState([])
+const [searchTransactions,setSearchTransaction] =useState("")
+useEffect(() => {
+  fetch("http://localhost:8001/transactions?q="+searchTransactions)
+    .then((resp) => resp.json())
+    .then(transaction => setTransaction(transaction))
+}, [searchTransactions])
+function handleSearch(e) {
+ setSearchTransaction(e.target.value)
+}
 
   
   return (
     <div>
-      <Search />
+      <Search handleSearch={handleSearch} />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transaction} />
     </div>
   );
 }
